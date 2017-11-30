e6e52bea8dd36540f9585e0c87aa15743f6beb69
ivanshe <ivanshe2009@126.com>
2017-11-30 21:33:07 +0800

    reset

--- bootserver/src/main/java/io/ivan/tpp/business/mycat/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package io.ivan.tpp.business.mycat.service;
-
-import io.ivan.tpp.business.mycat.entity.Hotel;
-
-import java.util.List;
-
-public interface OrderService {
-	public void addHotel(List<Hotel> hotels);
-}
