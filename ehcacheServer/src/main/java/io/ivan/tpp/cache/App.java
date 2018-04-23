f5751e510b91c3b71b95f5e93c18bf3c3225384d
shezhigao <shezhigao@trustrock.com.cn>
2018-04-23 10:04:29 +0800

    add commons

---
+++ ehcacheServer/src/main/java/io/ivan/tpp/cache/App.java
@@ -0,0 +1,13 @@
+package io.ivan.tpp.cache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
